e29aa8ac3dc4049cd33b68b7d3404219aa96417b
Ramu Ummadishetty <41884544+ramushetty@users.noreply.github.com>
2021-07-24 14:08:04 +0530

    Create userinput.py
    python input variables

---
+++ Python Tutorial/userinput.py
@@ -0,0 +1,7 @@
+inp = input("Enter here....") # by default it takes string data type
+inp1 = int(input("enter...")) # type cast string to int same like you can change any data type
+inp2 = float(input("enter float number ..."))
+
+print(f"{inp2:.2f}") # rounding to two decimals after period
+
+
